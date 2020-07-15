9b54ed7547c428352fd55505ce6531c6355e1347
ArnavSanghavi <arnav.sanghavi@gmail.com>
2020-07-15 16:17:04 +0530

    Inital Commit
    Adding all the files.

---
+++ Files/Scratch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scratch : MonoBehaviour
+{
+    public GameObject maskPrefab;
+    private bool isPressed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = 2;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+        if(isPressed == true)
+        { 
+            GameObject maskSprite = Instantiate(maskPrefab, mousePos, Quaternion.identity);
+            Invoke("reveal",10);
+            maskSprite.transform.parent = gameObject.transform;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            isPressed = true;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            isPressed = false;
+        }
+    }
+
+    void reveal()
+    {
+        Destroy(this.gameObject);
+    }
+}
